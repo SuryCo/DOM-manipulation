d8971aa0bf5ced0701ae021e810f8ce6b38513d5
SuryCo <surysblog.post@gmail.com>
2022-07-26 15:13:03 +0200

    Added some more tests and trials

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
 console.log("hello DOM");
+
+// let paragraph = document.getElementById('greeting');
+// console.log(paragraph);
+
+// let nestingTest = document.getElementById('nestingTest');
+// console.log(nestingTest);
+
+// document.body.innerHTML = '<h1> This is the heading now </h1>';
+
+document.getElementById('innerTest').innerHTML = '<h2> This is the heading now </h2>';
+
+console.log(document.getElementById('myParent').parentNode);
+console.log(document.getElementById('nestingTest').children);
